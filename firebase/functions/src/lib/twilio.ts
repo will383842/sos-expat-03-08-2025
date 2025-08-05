0639a46fb751f4d3d1347c0fc939c2e6823fdd5c
will383842 <williamjullin@gmail.com>
2025-08-05 19:56:33 +0200

    Sauvegarde automatique

--- firebase/functions/src/lib/twilio.ts
+++ firebase/functions/src/lib/twilio.ts
@@ -1,8 +1,15 @@
+// functions/src/lib/twilio.ts
+
+import * as functions from 'firebase-functions';
 import twilio from 'twilio';
 
-const client = twilio(
-  process.env.TWILIO_ACCOUNT_SID!,
-  process.env.TWILIO_AUTH_TOKEN!
-);
+// On récupère les identifiants depuis Firebase Functions Config
+const accountSid = functions.config().twilio.sid;
+const authToken = functions.config().twilio.token;
 
-export const twilioClient = client;
+if (!accountSid || !authToken) {
+  throw new Error('Twilio SID ou Token manquant dans les config Firebase.');
+}
+
+// Création du client Twilio
+export const twilioClient = twilio(accountSid, authToken);
